1c1972f7b8a45e70a95f68e3a6fbedb325755c9b
mostafa-LapTop <mostafakamel@esmaar.net>
2023-01-26 14:47:58 +0200

    tailwind apply directive for forms styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,60 +1,23 @@
 <template>
-  <header
-    class="
-      border-b border-gray-200
-      dark:border-gray-700
-      bg-white
-      dark:bg-gray-900
-      w-full
-    "
-  >
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
           <Link :href="route('listing.index')">Listings</Link>
         </div>
-        <div
-          class="
-            text-xl text-indigo-600
-            dark:text-indigo-300
-            font-bold
-            text-center
-          "
-        >
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div
-          class="
-            bg-indigo-600
-            hover:bg-indigo-500
-            text-white
-            font-medium
-            p-2
-            rounded-medium
-          "
-        >
-          <Link :href="route('listing.create')">+ New Listing</Link>
+        <div>
+          <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
         </div>
       </nav>
     </div>
   </header>
-  <main class="container max-auto p-4">
-    <div
-      v-if="flashSuccess"
-      class="
-        mb-4
-        border
-        rounded-md
-        shadow-sm
-        border-green-200
-        dark:border-green-800
-        bg-green-50
-        dark:bg-green-900
-        p-2
-      "
-    >
+  
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
-      i'm a flash !
     </div>
     <slot>Default</slot>
   </main>
@@ -65,5 +28,5 @@ import { computed } from "vue";
 import { Link, usePage } from "@inertiajs/vue3";
 
 const page = usePage();
-const flashSuccess = computed(() => page.props.flash.success);
+const flashSuccess = computed(() => page.props.flash.success)
 </script>
