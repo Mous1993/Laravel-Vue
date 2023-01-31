a3565033a2441f68f4dcb9e4601239b3bc7035fc
mostafa-LapTop <mostafakamel@esmaar.net>
2023-01-31 15:22:05 +0200

    associate listings with specific user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,13 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-
-        \App\Models\Listing::factory(20)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'test2@example.com',
+        ]);
+        \App\Models\Listing::factory(20)->create([
+            'by_user_id' => 1
+        ]);
 
     }
 }
